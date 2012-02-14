a05c168b980cb9ddaebecd90995a0347f70cb390
Ismayil Khayredinov <ismayil.khayredinov@hypejunction.com>
2012-02-14 14:31:33 +0100

    fixes language strings for actions

--- actions/follow.php
+++ actions/follow.php
@@ -9,14 +9,14 @@ $object_guid = get_input('object_guid');
 $object = get_entity($object_guid);
 
 if (!$object instanceof ElggEntity) {
-	register_error("Could not find the specified entity.  It may have been deleted or you may not have access to it");
+	register_error(elgg_echo('followers:follow:error:noentity'));
 	forward(REFERER);
 }
 
 if (add_entity_relationship(elgg_get_logged_in_user_guid(), 'follower', $object_guid)) {
-	system_message(elgg_echo("You are now following %s", array($object->name)));	
+	system_message(elgg_echo('followers:follow:success', array($object->name)));
 } else {
-	register_error(elgg_echo("Something went wrong when attempting to follow %s", array($object->name)));
+	register_error(elgg_echo('followers:follow:error:unknown', array($object->name)));
 }
 
 forward(REFERER);
--- languages/en.php
+++ languages/en.php
@@ -9,4 +9,15 @@ add_translation('en', array(
 
 	'widgets:followers:name' => 'Followers',
 	'widgets:followers:description' => 'A list of your followers',
+
+
+	/* Actions */
+	'followers:follow:error:noentity' => 'Could not find the specified entity.  It may have been deleted or you may not have access to it',
+	'followers:follow:error:unknown' => 'Something went wrong when attempting to follow %s',
+	'followers:follow:success' => 'You are now following %s',
+
+	'followers:unfollow:error:noentity' => 'Could not find the specified entity.  It may have been deleted or you may not have access to it',
+	'followers:unfollow:error:unknown' => 'Something went wrong when attempting to unfollow %s',
+	'followers:unfollow:success' => 'You are no longer following %s',
+
 ));
